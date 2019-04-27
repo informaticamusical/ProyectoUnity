8044cb0809979a2e7581f64cc900d33d7b822f3e
Diego BV <diego.baratto8@gmail.com>
2019-04-27 14:38:48 +0200

    Adaptado Conductor

--- InformaticaMusical/Assets/Scripts/Conductor.cs
+++ InformaticaMusical/Assets/Scripts/Conductor.cs
@@ -2,38 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
+[System.Serializable]
 public class ConductorData
 {
     public double bpm = 120;
-    public double crotchet;
-    public double offset = 0.2d;
-    public double songPosition;
-    public double dpsin;
-    AudioSource song;
+    public AudioSource song;
+
+    private double songPosition;
+    private double offset = 0.2d;
+    private double dpsin; //init time
+    private double crotchet;
     
-    public ConductorData(AudioSource audioSource) { song = audioSource; crotchet = (60 / bpm) * 4; dpsin = AudioSettings.dspTime; }
+    public void init() { crotchet = (60 / bpm); dpsin = AudioSettings.dspTime; }
 
     public void update() { songPosition = (AudioSettings.dspTime - dpsin)*song.pitch - offset; /*(song.time) * song.pitch - offset*/; }
-}
 
-public class Conductor : MonoBehaviour {
-
-    ConductorData c;
-    AudioSource song;
-
-	// Use this for initialization
-	void Awake () {
-        song = GetComponent<AudioSource>();
-        c = new ConductorData(song);
-    }
-	
-	// Update is called once per frame
-	void Update () {
-        c.update();
-	}
-
-    public ConductorData GetConductorData(){
-        return c;
-    }
+    public double getCrotchet() { return crotchet; }
+    public double getSongPosition() { return songPosition; }
 }
--- InformaticaMusical/Assets/Scripts/FollowBeat.cs
+++ InformaticaMusical/Assets/Scripts/FollowBeat.cs
@@ -3,24 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FollowBeat : MonoBehaviour {
+    public ConductorData c; //un conductor por ritmo. Si todos siguen el ritmo de la cancion de fondo, solo habria un conductor, se tendria que cambiar el multiplicador por cada tipo de enemigo
+    public double multiplier = 4;
     double lastBeat;
-    ConductorData c = null;
+
 	// Use this for initialization
 	void Start () {
         lastBeat = 0.0d;
-        c = this.gameObject.GetComponent<Conductor>().GetConductorData();
+        c.init();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        c.update(); //deberia estar en el manager de los enemigos
+
         //Me tengo que mover
-		if(c.songPosition > lastBeat + c.crotchet)
+		if(c.getSongPosition() > lastBeat + c.getCrotchet() * multiplier)
         {
             //do something
             this.gameObject.transform.localScale = new Vector3(this.gameObject.transform.localScale.x + 1, this.gameObject.transform.localScale.y+1, this.gameObject.transform.localScale.z+1);
             this.GetComponent<Renderer>().material.color = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
 
-            lastBeat += c.crotchet;
+            lastBeat += c.getCrotchet()* multiplier;
         }
 	}
 }
